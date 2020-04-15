fdbee89a5bbaa648d03d4b762fc5ad46a2a2e0ac
Robertus Dwi Ari Utomo <robertus.ari.utomo@gmail.com>
2020-04-15 14:30:33 +0700

    Update the logic of AudioLoop
    - Adding callback to AudioLoop so, when it called the callback is assign and always checking every frame to make sure the audio will loop based given start and end time.
    - Change the  AudioLoop object created, now it can be used by other object, not only single object. Each object will have its own callback.
    - Calling the AudioLoop can't anymore by only call the class, but we have to define the class variable and assign it first before using it.

--- AudioLoop.cs
+++ AudioLoop.cs
@@ -1,18 +1,74 @@
-﻿using System;
+﻿// AudioLoop version 1.1
+using System;
 using UnityEngine;
 
 namespace TomWill
-{
-    public static class AudioLoop 
+{ 
+    public class AudioLoop
     {
+        public enum LoopType { LOOP_WITH_INTRO, SPECIFIC_LOOP, LOOP_EXCEPTION, NONE }
+        private LoopType currentLoopType = LoopType.NONE;
+
+        private AudioLoopCallbacks callbacks;
+        private AudioSource source;
+
+        private float start;
+        private float end;
+
+        /// <summary>
+        /// Set the audio source to define where the audio source being affected by this AudioLoop function
+        /// </summary>
+        /// <param name="source">Is the audio source will be modify to loop the music</param>
+        public void SetAudioSource(ref AudioSource source)
+        {
+            this.source = source;
+            if (callbacks == null) callbacks = source.transform.gameObject.AddComponent<AudioLoopCallbacks>();
+        }
+
+        /// <summary>
+        /// Playing audio with loop option based on given start and end time to loops
+        /// </summary>
+        /// <param name="loopType">
+        ///     Loop option, to loop the audio with different option
+        ///     <br> - LOOP_WITH_INTRO : Is the loop option to make looping at specific track session with intro first time </br>
+        ///     <br> - SPECIFIC_LOOP : Is the loop option to make looping at specific track session without intro at the first play </br>
+        ///     <br> - LOOP_EXCEPTION : Is the loop option that loop the entire audio clip except the given value of start time until end time </br>
+        ///     <br> - NONE : Is the none option, doesn't use the Audio Loop option, but use the AudioSource option. </br>
+        ///     <br></br>
+        ///     <br>Note : Intro is the first track time on audio clip before the start value.</br>
+        /// </param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        public void PlayLoops(LoopType loopType, float start, float end)
+        {
+            this.start = start;
+            this.end = end;
+
+            if (currentLoopType != loopType)
+            {
+                currentLoopType = loopType;
+
+                switch (currentLoopType)
+                {
+                    case LoopType.LOOP_WITH_INTRO:
+                        callbacks.SetCallbacks(OnLoopWithIntro_Callbacks);
+                        break;
+                    case LoopType.SPECIFIC_LOOP:
+                        callbacks.SetCallbacks(OnLoop_Callbacks);
+                        break;
+                    case LoopType.LOOP_EXCEPTION:
+                        callbacks.SetCallbacks(PlayExcept_Callbacks);
+                        break;
+                }
+            }
+            
+        }
+
         /// <summary>
         /// This is the method to make looping at specific track session with intro first time
         /// Will be seen when the start loop time is not the start time of the clip
         /// </summary>
-        /// <param name="source"> Is the Audio Source you want to loop with clip attached </param>
-        /// <param name="startLoop"> Is the time where you want to start the loop in seconds </param>
-        /// <param name="endLoop"> Is the time where the audio will get back to start loop time in seconds </param>
-        public static void OnLoopWithIntro(ref AudioSource source, float startLoop, float endLoop)
+        private void OnLoopWithIntro_Callbacks()
         {
             try
             {
@@ -23,22 +79,26 @@ namespace TomWill
 
                 if (!source.isPlaying) source.Play();
 
-                if (CheckTimeLoop(source, startLoop, endLoop))
+                if (CheckTimeLoop())
                 {
                     source.Stop();
                     return;
                 }
 
-                if (clipTime >= endLoop)
+                if (clipTime >= end)
                 {
-                    source.time = startLoop;
+                    source.time = start;
                 }
-            } catch (NullReferenceException er)
+            }
+            catch (NullReferenceException er)
             {
-                Debug.LogWarning("The Audio Source is dont have clip attached");
-            } catch (UnassignedReferenceException erRefference)
+                Debug.LogWarning("The Audio Source is dont have clip attached" + er.Message);
+                callbacks.Unsubscribe(OnLoopWithIntro_Callbacks);
+            }
+            catch (UnassignedReferenceException erRefference)
             {
-                Debug.LogWarning("The Audio Source is null");
+                Debug.LogWarning("The Audio Source is null" + erRefference.Message);
+                callbacks.Unsubscribe(OnLoopWithIntro_Callbacks);
             }
         }
 
@@ -46,10 +106,7 @@ namespace TomWill
         /// This is the method to make looping at specific track session without intro music, 
         /// The audio will playing between start loop time and end loop time
         /// </summary>
-        /// <param name="source"> Is the Audio Source you want to loop with clip attached </param>
-        /// <param name="startLoop"> Is the time where you want to start the loop in seconds </param>
-        /// <param name="endLoop"> Is the time where the audio will get back to start loop time in seconds </param>
-        public static void OnLoop(ref AudioSource source, float startLoop, float endLoop)
+        public void OnLoop_Callbacks()
         {
             try
             {
@@ -60,68 +117,67 @@ namespace TomWill
 
                 if (!source.isPlaying) source.Play();
 
-                if (CheckTimeLoop(source, startLoop, endLoop))
+                if (CheckTimeLoop())
                 {
                     source.Stop();
                     return;
                 }
 
-                if (clipTime < startLoop)
+                if (clipTime < start)
                 {
-                    source.time = startLoop;
+                    source.time = start;
                 }
 
-                if (clipTime >= endLoop)
+                if (clipTime >= end)
                 {
-                    source.time = startLoop;
+                    source.time = start;
                 }
             }
             catch (NullReferenceException er)
             {
-                Debug.LogWarning("The Audio Source is dont have clip attached");
+                Debug.LogWarning("The Audio Source is dont have clip attached : " + er.Message);
+                callbacks.Unsubscribe(OnLoop_Callbacks);
             }
             catch (UnassignedReferenceException erRefference)
             {
-                Debug.LogWarning("The Audio Source is null");
+                Debug.LogWarning("The Audio Source is null : " + erRefference.Message);
+                callbacks.Unsubscribe(OnLoop_Callbacks);
             }
         }
 
         /// <summary>
         /// This method will check the exception value for start loop time and end loop time
-        /// - Max time is the length time of clip in seconds
-        /// - Start Loop time is not allowed to be more than Max Time and End Loop Time
-        /// - Start Loop time is not allowed to be same with End Loop Time
-        /// - End Loop time is not allowed to be more than Max Time
+        /// <br> - Max time is the length time of clip in seconds </br>
+        /// <br> - Start Loop time is not allowed to be more than Max Time and End Loop Time </br>
+        /// <br> - Start Loop time is not allowed to be same with End Loop Time </br>
+        /// <br> - End Loop time is not allowed to be more than Max Time </br>
         /// </summary>
-        /// <param name="source"> Is the Audio Source you want to loop with clip attached </param>
-        /// <param name="startLoop"> Is the time where you want to start the loop in seconds </param>
-        /// <param name="endLoop"> Is the time where the audio will get back to start loop time in seconds </param>
         /// <returns> Returns true if the above conditions are met and returns false if none of the above conditions are met</returns>
-        private static bool CheckTimeLoop(AudioSource source, float startLoop, float endLoop)
+        private bool CheckTimeLoop()
         {
             float maxTime = source.clip.length;
-            if (maxTime < startLoop)
+            if (maxTime < start)
             {
                 Debug.LogWarning("Start Loop Time is more than the maximum time in Clip played");
-                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nStart loop time is " + startLoop + " seconds");
+                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nStart loop time is " + start + " seconds");
                 return true;
             }
-            else if (startLoop > endLoop)
+            else if (start > end)
             {
                 Debug.LogWarning("Start Loop Time is more than the End Loop time");
-                Debug.LogWarning("End Loop time is " + endLoop + " seconds\nStart loop time is " + startLoop + " seconds");
+                Debug.LogWarning("End Loop time is " + end + " seconds\nStart loop time is " + start + " seconds");
                 return true;
             }
-            else if (maxTime < endLoop)
+            else if (maxTime < end)
             {
                 Debug.LogWarning("End Loop Time is more than the maximum time in Clip played");
-                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nEnd loop time is " + startLoop + " seconds");
+                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nEnd loop time is " + start + " seconds");
                 return true;
             }
-            else if (startLoop == endLoop)
+            else if (start == end)
             {
                 Debug.LogWarning("Start Loop Time has same value with End Loop time, will make this function is useless");
-                Debug.LogWarning("End Loop time is " + endLoop + " seconds\nStart loop time is " + startLoop + " seconds");
+                Debug.LogWarning("End Loop time is " + end + " seconds\nStart loop time is " + start + " seconds");
                 return true;
             }
             else return false;
@@ -131,10 +187,7 @@ namespace TomWill
         /// This is the method to make exception play between start except time (exclude) and end except time (excude)
         /// Will do the looping other than the specified time range
         /// </summary>
-        /// <param name="source"> Is the Audio Source you want to loop with clip attached </param>
-        /// <param name="startExcept"> Is the time where you want to start the exception play (exclude) </param>
-        /// <param name="endExcept"> Is the time where the audio will skip from the start time untill this time (exclude) </param>
-        public static void PlayExcept(ref AudioSource source, float startExcept, float endExcept)
+        public void PlayExcept_Callbacks()
         {
             try
             {
@@ -153,70 +206,69 @@ namespace TomWill
                     source.Play();
                 }
 
-                if (CheckTimeExcept(source, startExcept, endExcept))
+                if (CheckTimeExcept())
                 {
                     source.Stop();
                     return;
                 }
 
-                if (clipTime >= startExcept && clipTime < endExcept)
+                if (clipTime >= start && clipTime < end)
                 {
-                    source.time = endExcept;
+                    source.time = end;
                 }
 
             }
             catch (NullReferenceException er)
             {
-                Debug.LogWarning("The Audio Source is dont have clip attached");
+                Debug.LogWarning("The Audio Source is dont have clip attached" + er.Message);
+                callbacks.Unsubscribe(PlayExcept_Callbacks);
             }
             catch (UnassignedReferenceException erRefference)
             {
-                Debug.LogWarning("The Audio Source is null");
+                Debug.LogWarning("The Audio Source is null" + erRefference.Message);
+                callbacks.Unsubscribe(PlayExcept_Callbacks);
             }
         }
 
         /// <summary>
         /// This method will check the exception value for start except time and end except time
-        /// - Max time is the length time of clip in seconds
-        /// - End Except time is not allowed to be same or more than Max Time
-        /// - Start Except time is not allowed to be more than Max Time and End Except Time
-        /// - Start Except time is not allowed to be same with End Except Time
+        /// <br>- Max time is the length time of clip in seconds </br>
+        /// <br>- End Except time is not allowed to be same or more than Max Time </br>
+        /// <br>- Start Except time is not allowed to be more than Max Time and End Except Time </br>
+        /// <br>- Start Except time is not allowed to be same with End Except Time </br>
         /// </summary>
-        /// <param name="source"> Is the Audio Source you want to loop with clip attached </param>
-        /// <param name="startExcept"> Is the time where you want to start the exception play (exclude) </param>
-        /// <param name="endExcept"> Is the time where the audio will skip from the start time untill this time (exclude) </param>
         /// <returns> Returns true if the above conditions are met and returns false if none of the above conditions are met</returns>
-        private static bool CheckTimeExcept(AudioSource source, float startExcept, float endExcept)
+        private bool CheckTimeExcept()
         {
             float maxTime = source.clip.length;
-            if (maxTime < startExcept)
+            if (maxTime < start)
             {
                 Debug.LogWarning("Start Except Time is more than the maximum time in Clip played");
-                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nStart Except time is " + startExcept + " seconds");
+                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nStart Except time is " + start + " seconds");
                 return true;
             }
-            else if (startExcept > endExcept)
+            else if (start > end)
             {
                 Debug.LogWarning("Start Except Time is more than the End Except time");
-                Debug.LogWarning("End Except time is " + endExcept + " seconds\nStart Except time is " + startExcept + " seconds");
+                Debug.LogWarning("End Except time is " + end + " seconds\nStart Except time is " + start + " seconds");
                 return true;
             }
-            else if (maxTime < endExcept)
+            else if (maxTime < end)
             {
                 Debug.LogWarning("End Except Time is more than the maximum time in Clip played");
-                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nEnd except time is " + startExcept + " seconds");
+                Debug.LogWarning("You are playing " + source.clip.name + "(" + source.clip.length + "seconds)\nEnd except time is " + start + " seconds");
                 return true;
             }
-            else if (startExcept == endExcept)
+            else if (start == end)
             {
                 Debug.LogWarning("Start Except Time has same value with End Except time, will make this function is useless");
-                Debug.LogWarning("End except time is " + endExcept + " seconds\nStart except time is " + startExcept + " seconds");
+                Debug.LogWarning("End except time is " + end + " seconds\nStart except time is " + start + " seconds");
                 return true;
             }
-            else if (maxTime == endExcept)
+            else if (maxTime == end)
             {
                 Debug.LogWarning("End Except Time is not allowed to be same with the Max Time Clip");
-                Debug.LogWarning("Max time is " + maxTime + " seconds\nEnd except time is " + endExcept + " seconds");
+                Debug.LogWarning("Max time is " + maxTime + " seconds\nEnd except time is " + end + " seconds");
                 return true;
             }
             else return false;
---
+++ AudioLoopCallbacks.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioLoopCallbacks : MonoBehaviour
+{
+    public delegate void ActiveCallbacks();
+    private ActiveCallbacks activeCallbacks;
+
+    private void Update()
+    {
+        activeCallbacks?.Invoke();
+    }
+
+    /// <summary>
+    /// Set the CurrentCallbacks to make the Audioloop always looping
+    /// </summary>
+    /// <param name="_activeCallbacks">The callback method to call every single frame</param>
+    public void SetCallbacks(ActiveCallbacks _activeCallbacks)
+    {
+        activeCallbacks = _activeCallbacks;
+    }
+
+    public void Unsubscribe(ActiveCallbacks _activeCallbacks)
+    {
+        if (_activeCallbacks == activeCallbacks) activeCallbacks = null;
+    }
+}
